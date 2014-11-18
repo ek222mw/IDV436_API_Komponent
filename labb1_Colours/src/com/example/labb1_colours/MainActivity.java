9941a6351b264215ec79fb4aa38213ea1cf6f3b0
Emil Karlsson <ek222mw@student.lnu.se>
2014-11-18 18:07:29 +0100

    Uppdatering labb1, fixat utseende portrait upp3

--- labb1_Colours/src/com/example/labb1_colours/MainActivity.java
+++ labb1_Colours/src/com/example/labb1_colours/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.labb1_colours;
 
 import android.app.Activity;
+import android.content.res.Configuration;
 import android.graphics.Color;
+import android.hardware.SensorManager;
 
-
+import android.view.OrientationEventListener;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -11,51 +13,93 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class MainActivity extends Activity {
 
-    @Override
+	OrientationEventListener myOrientationEventListener;
+	public boolean flag;
+    
+	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+       
+       
+       // setContentView(R.layout.activity_main);
+       
+       
+       myOrientationEventListener
+       = new OrientationEventListener(this, SensorManager.SENSOR_DELAY_NORMAL){
+
+        @Override
+        public void onOrientationChanged(int arg0) {
+         // TODO Auto-generated method stub
+        	
+         
+        }};
+        
+          if (myOrientationEventListener.canDetectOrientation()){
+        	  setContentView(R.layout.activity_main);
+           myOrientationEventListener.enable();
+           
+          }
+          else{
+        	  setContentView(R.layout.activity_main);
+           finish();
+          }
+    
+       }
+       
+    @Override
+    protected void onDestroy() {
+     // TODO Auto-generated method stub
+     super.onDestroy();
+     myOrientationEventListener.disable();
     }
+   
+   
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
-        
-        setContentView(R.layout.activity_main);
-        
-        final Button button = (Button) findViewById(R.id.Button_Send);
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-            	EditText et_red = (EditText) findViewById(R.id.edit_text_red);
-            	EditText et_green = (EditText) findViewById(R.id.edit_text_green);
-            	EditText et_blue = (EditText) findViewById(R.id.edit_text_blue);
-            	
-            	TextView tv = (TextView) findViewById(R.id.result);
-            	
-            	int inputRed = Integer.valueOf(et_red.getText().toString());
-            	int inputGreen = Integer.valueOf(et_green.getText().toString());
-            	int inputBlue = Integer.valueOf(et_blue.getText().toString());
-            	
-            	
-            	//int color = Color.rgb(inputRed, inputGreen, inputBlue);
-            	
-            	
-            	
-            	tv.setBackgroundColor(Color.rgb(inputRed, inputGreen, inputBlue));
-            	
-            	
-        		
-            }
-        });
+     // Checks the orientation of the screen
+       
+            
+            
+            final Button button = (Button) findViewById(R.id.Button_Send);
+            button.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                	EditText et_red = (EditText) findViewById(R.id.edit_text_red);
+                	EditText et_green = (EditText) findViewById(R.id.edit_text_green);
+                	EditText et_blue = (EditText) findViewById(R.id.edit_text_blue);
+                	
+                	TextView tv = (TextView) findViewById(R.id.result);
+                	
+                	int inputRed = Integer.valueOf(et_red.getText().toString());
+                	int inputGreen = Integer.valueOf(et_green.getText().toString());
+                	int inputBlue = Integer.valueOf(et_blue.getText().toString());
+                	
+                	
+                	//int color = Color.rgb(inputRed, inputGreen, inputBlue);
+                	
+                	
+                	
+                	tv.setBackgroundColor(Color.rgb(inputRed, inputGreen, inputBlue));
+                	
+                	
+            		
+                }
+            });
+            
+       
+       
         
         return true;
-    }
+        }
+    
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
